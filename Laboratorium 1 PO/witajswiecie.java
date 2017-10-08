6cb50945dc6e121e2fd89cce28074ccf2e5cc683
Mateusz Ziomek <mziomek108@gmail.com>
2017-10-08 20:15:53 +0200

    Lab1 - exercises
    No solutions

---
+++ Laboratorium 1 PO/witajswiecie.java
@@ -0,0 +1,12 @@
+/**
+ * Klasa wyœwietlajaca powitaniena konsoli.
+ */
+public class HelloWorld {
+    /**
+     *
+     * @param argv lista argumentów przekazywana do programu
+     */
+   public static void main(String [] argv){
+       System.out.println("Hello World!");
+  }
+}
