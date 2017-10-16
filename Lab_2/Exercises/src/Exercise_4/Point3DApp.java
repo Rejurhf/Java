2615ebeb7f2814a1721c5d143ef8fb95dd6e6e78
Mateusz Ziomek <mziomek108@gmail.com>
2017-10-16 09:42:59 +0200

    problem

--- Lab_2/Exercises/src/Exercise_4/Point3DApp.java
+++ Lab_2/Exercises/src/Exercise_4/Point3DApp.java
@@ -2,11 +2,11 @@ package Exercise_4;
 
 public class Point3DApp{
     public static void main(String[] argv){
-        Point2D p1 = new Point2D(4, 1);
-        Point2D p2 = new Point2D(1, 4);
+        Point2D p1 = new Point2D(1, 2);
+        Point2D p2 = new Point2D(2, 1);
         System.out.println("Distance between p1 and p2: " + p1.distance(p2));
-        Point3D p3 = new Point3D(0, 3, 0);
-        Point3D p4 = new Point3D(4, 0, 1);
+        Point3D p3 = new Point3D(1, 2, 3);
+        Point3D p4 = new Point3D(3, 2, 1);
         System.out.println("Distance between p3 and p4: " + p3.distance(p4));
     }
 }
