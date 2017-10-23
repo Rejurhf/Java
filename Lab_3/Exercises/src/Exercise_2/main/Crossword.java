f6ac2bd2bad7baf98536649f5f2d59752c96bdec
Mateusz Ziomek <mziomek108@gmail.com>
2017-10-23 20:21:21 +0200

    Problem Fixed

--- Lab_3/Exercises/src/Exercise_2/main/Crossword.java
+++ Lab_3/Exercises/src/Exercise_2/main/Crossword.java
@@ -4,12 +4,16 @@ import java.util.LinkedList;
 public class Crossword{
     public static void main(String[] argv){
         InteliCwDB crosswordPuzzle = new InteliCwDB("src/Exercise_2/resources/Crossword.txt");
+
         LinkedList<Entry> wordsThatMatches = new LinkedList<>();
         wordsThatMatches = crosswordPuzzle.fileAll("ata");
-
-        for (Entry word: wordsThatMatches){
+        for (Entry word:wordsThatMatches){
             System.out.println(word.getWord());
             //System.out.println(word.getClue());
         }
+
+        System.out.println("\n" + crosswordPuzzle.getRandom().getWord());
+        System.out.println("\n" + crosswordPuzzle.getRandom(5).getWord());
+        System.out.println("\n" + crosswordPuzzle.getRandom("ata").getWord());
     }
 }
