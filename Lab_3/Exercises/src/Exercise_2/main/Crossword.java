fed2d07126e92fb855e35707762d998dff671cef
Mateusz Ziomek <mziomek108@gmail.com>
2017-10-23 20:34:52 +0200

    Exe_2 - Done

--- Lab_3/Exercises/src/Exercise_2/main/Crossword.java
+++ Lab_3/Exercises/src/Exercise_2/main/Crossword.java
@@ -7,13 +7,14 @@ public class Crossword{
 
         LinkedList<Entry> wordsThatMatches = new LinkedList<>();
         wordsThatMatches = crosswordPuzzle.fileAll("ata");
+        System.out.println(">: Słowa pasujące do wzorca \"ata\":");
         for (Entry word:wordsThatMatches){
             System.out.println(word.getWord());
             //System.out.println(word.getClue());
         }
 
-        System.out.println("\n" + crosswordPuzzle.getRandom().getWord());
-        System.out.println("\n" + crosswordPuzzle.getRandom(5).getWord());
-        System.out.println("\n" + crosswordPuzzle.getRandom("ata").getWord());
+        System.out.println(">: Losowe słowo:\n" + crosswordPuzzle.getRandom().getWord());
+        System.out.println(">: Losowe słowo o długości 5:\n" + crosswordPuzzle.getRandom(5).getWord());
+        System.out.println(">: Losowe słowo pasujące do wzorca \"ata\"\n" + crosswordPuzzle.getRandom("ata").getWord());
     }
 }
