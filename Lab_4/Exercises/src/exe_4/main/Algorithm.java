e4d45d94b8de4d8f38cf2ae06b7acb535bb069a9
Mateusz Ziomek <mziomek108@gmail.com>
2017-10-25 19:12:39 +0200

    Crypt/Decrypt - InProgres

---
+++ Lab_4/Exercises/src/exe_4/main/Algorithm.java
@@ -0,0 +1,6 @@
+package exe_4.main;
+
+interface Algorithm{
+    public String crypt(String wordToCrypt);
+    public String decrypt(String wordToDecrypt);
+}
---
+++ Lab_4/Exercises/src/exe_4/main/Polibiusz.java
@@ -0,0 +1,12 @@
+package exe_4.main;
+
+public class Polibiusz implements Algorithm{
+    @Override
+    public String crypt(String wordToCrypt) {
+        return null;
+    }
+    @Override
+    public String decrypt(String wordToDecrypt) {
+        return null;
+    }
+}
