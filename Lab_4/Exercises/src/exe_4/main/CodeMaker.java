24010a43ff546f9c377c693cd5783b7e86a9d6ed
Mateusz Ziomek <mziomek108@gmail.com>
2017-10-28 22:06:36 +0200

    ROT11 Done

--- Lab_4/Exercises/src/exe_4/main/CodeMaker.java
+++ Lab_4/Exercises/src/exe_4/main/CodeMaker.java
@@ -9,10 +9,6 @@ public class CodeMaker{
         File fileToSaveCrypt = new File(pathToResrc + "fileToSaveCript.txt");
         File fileToDecrypt = new File(pathToResrc + "fileToDecript.txt");
         File fileToSaveDecrypt = new File(pathToResrc + "fileToSaveDecript.txt");
-
-        String a = "abc";
-        System.out.println(a);
-        a = a.substring(0, 1) + "g" + a.substring(1 + 1);
-        System.out.println(a);
+        
     }
 }
--- Lab_4/Exercises/src/exe_4/main/ROT11.java
+++ Lab_4/Exercises/src/exe_4/main/ROT11.java
@@ -3,16 +3,31 @@ package exe_4.main;
 public class ROT11 implements Algorithm{
     private static int move = 11;                               //a-z i A-Z te znaki są szyfrowane
 
-    public String crypt(String wordToCrypt){
-
-        return wordToCrypt;
+    public String crypt(String wordToCrp){
+        String crpWord = "";
+        int rot11 = 11, albhbtLen = 26;
+        for(int i = 0; i < wordToCrp.length(); i++) {
+            char ch = wordToCrp.charAt(i);
+            if(ch >= 'a' && ch <= 'o') ch += rot11;
+                else if(ch >= 'A' && ch <= 'O') ch += rot11;
+                else if(ch >= 'p' && ch <= 'z') ch -= albhbtLen - rot11;
+                else if(ch >= 'P' && ch <= 'Z') ch -= albhbtLen - rot11;
+            crpWord += ch;
+        }
+        return crpWord;
     }
 
-    public String decrypt(String wordToDecrypt){
-        return wordToDecrypt;
-    }
-
-    public static String replaceCharAt(String s, int pos, char c) {
-        return s.substring(0, pos) + c + s.substring(pos + 1);
+    public String decrypt(String wordToDecrp){
+        String decrpWord = "";
+        int rot11 = 11, albhbtLen = 26;
+        for(int i = 0; i < wordToDecrp.length(); i++){
+            char ch = wordToDecrp.charAt(i);
+            if(ch >= 'a' && ch < 'k') ch += albhbtLen - rot11;
+                else if(ch >= 'A' && ch <= 'K') ch += albhbtLen - rot11;
+                else if(ch >= 'l' && ch < 'z') ch -= rot11;
+                else if(ch >= 'L' && ch <= 'Z') ch -= rot11;
+            decrpWord += ch;
+        }
+        return decrpWord;
     }
 }
