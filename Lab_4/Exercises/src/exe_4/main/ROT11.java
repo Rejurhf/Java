3a3acd3a2a4540d6f4acdaee7122447e3fa8b02b
Mateusz Ziomek <mziomek108@gmail.com>
2017-10-28 14:17:08 +0200

    rot11 Partli Done

--- Lab_4/Exercises/src/exe_4/main/ROT11.java
+++ Lab_4/Exercises/src/exe_4/main/ROT11.java
@@ -1,13 +1,18 @@
 package exe_4.main;
 
 public class ROT11 implements Algorithm{
-    private static int move = 11; //a-z i A-Z te znaki są szyfrowane
+    private static int move = 11;                               //a-z i A-Z te znaki są szyfrowane
 
     public String crypt(String wordToCrypt){
+
         return wordToCrypt;
     }
 
     public String decrypt(String wordToDecrypt){
         return wordToDecrypt;
+    }
+
+    public static String replaceCharAt(String s, int pos, char c) {
+        return s.substring(0, pos) + c + s.substring(pos + 1);
     }
 }
