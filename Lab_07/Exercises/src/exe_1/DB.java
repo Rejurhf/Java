64b9686cac58d5ce6a0c596285b681032f38f891
Mateusz Ziomek <mziomek108@gmail.com>
2017-11-19 21:19:17 +0100

    Books Done
    Everything done with books

--- Lab_07/Exercises/src/exe_1/DB.java
+++ Lab_07/Exercises/src/exe_1/DB.java
@@ -19,9 +19,18 @@ public class DB{
         }catch (Exception e){ e.printStackTrace(); }
     }
 
-    public void listNames(){
+    public void disconnect(){
+        if(conn != null){
+            try {
+                conn.close();
+            }catch (SQLException e){ }
+        }
+        stmt = null;
+        rs = null;
+    }
+
+    public void listTitles(){
         try{
-            connect();
             stmt = conn.createStatement();
             rs = stmt.executeQuery("SELECT * FROM books");
 
@@ -42,12 +51,107 @@ public class DB{
             stmt = null;
         }
     }
-/*
-    public void createTable(){
-        stmt = conn.createStatement();
-        stmt.executeUpdate(
-                "CREATE TABLE tablea1(" + "priKey"
-        )
+
+    public void showAll(){
+        try{
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM books");
+
+            while (rs.next()){
+                String isbn = rs.getString(1);
+                String title = rs.getString(2);
+                String author = rs.getString(3);
+                String year = rs.getString(4);
+                int addSpace = 31 - title.length();
+                System.out.println(isbn + " | " + title + "   |   " + author + "  |  " + year);
+            }
+            System.out.println();
+        }catch (SQLException e){
+            System.out.println("SQLExeption: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }finally {
+            if (rs != null){
+                try{
+                    rs.close();
+                }catch (SQLException e){ }
+            }
+            stmt = null;
+        }
     }
-*/
+
+    public void findByIsbn(String toFind, int index){
+        try{
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM books");
+            boolean flag = false;
+
+            while (rs.next()){
+                //System.out.println(rs.getString(index));
+                if(toFind.equals(rs.getString(index))){
+                    flag = true;
+                    System.out.println(rs.getString(1) + " " + rs.getString(2) +
+                            " " + rs.getString(3) + " " + rs.getString(4));
+                }
+            }
+            if(!flag && index == 1)
+                System.out.println("There is no such isbn (" + toFind + ").");
+            else if (!flag && index == 3)
+                System.out.println("There is no such author (" + toFind + ").");
+        }catch (SQLException e){
+            System.out.println("SQLExeption: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }finally {
+            if (rs != null){
+                try{
+                    rs.close();
+                }catch (SQLException e){ }
+            }
+            stmt = null;
+        }
+    }
+
+    public void addBook(String newIsbn, String newTitle, String newAuthor, int newYear){
+        try{
+            stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO books(isbn, title, author, year) " +
+                    "VALUE ('"+newIsbn+"','"+newTitle+"','"+newAuthor+"','"+newYear+"')");
+        }catch (SQLException e){
+            System.out.println("SQLExeption: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }finally {
+            if (rs != null){
+                try{
+                    rs.close();
+                }catch (SQLException e){ }
+            }
+            stmt = null;
+        }
+    }
+
+    public void delBook(String itemToDel, int index){
+        try{
+            stmt = conn.createStatement();
+            if(index == 1){
+                stmt.executeUpdate("DELETE FROM books WHERE isbn = '"+itemToDel+"'");
+                System.out.println("Book with isbn: " + itemToDel + " deleted.\n");
+            }else if (index == 2){
+                stmt.executeUpdate("DELETE FROM books WHERE author = '"+itemToDel+"'");
+                System.out.println(itemToDel + " deleted.\n");
+            }
+        }catch (SQLException e){
+            System.out.println("SQLExeption: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }finally {
+            if (rs != null){
+                try{
+                    rs.close();
+                }catch (SQLException e){ }
+            }
+            stmt = null;
+        }
+    }
 }
--- Lab_07/Exercises/src/exe_1/DBApp.java
+++ Lab_07/Exercises/src/exe_1/DBApp.java
@@ -4,6 +4,17 @@ public class DBApp{
     public static void main(String[] argv){
         DB data = new DB();
         data.connect();
-        data.listNames();
+
+        //data.listTitles();
+        //data.showAll();
+        //data.findByIsbn("Fiodor Dostojewski", 3);
+        data.showAll();
+        data.addBook("1234567899277", "Miecz Przeznaczenia",
+         "Andrzej Sapkowski", 1994);
+        data.showAll();
+
+        data.delBook("Andrzej Sapkowski", 2);
+        data.showAll();
+        data.disconnect();
     }
 }
