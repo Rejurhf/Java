ade321e3f03535b3dc324d590345009cbdf41110
Mateusz Ziomek <mziomek108@gmail.com>
2017-12-03 17:03:40 +0100

    Threds Test Done

--- Lab_09/Exercises/src/exe_0/Chef.java
+++ Lab_09/Exercises/src/exe_0/Chef.java
@@ -1,18 +1,26 @@
 package exe_0;
 
 class Chef extends Thread {
-    WaitPerson p1, p2;
     int time;
+    int count = 0;
 
     Chef(int time){
-        System.out.println("Start");
+        this.time = time;
+        System.out.println("Start time: " + time);
         start();
     }
 
     @Override
     public void run() {
         super.run();
-        while(true) {
+        while(true){
+            try{
+                count++;
+                sleep(time);
+                System.out.println("Time: " + time + " count: " + count);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
 
         }
     }
--- Lab_09/Exercises/src/exe_0/Main.java
+++ Lab_09/Exercises/src/exe_0/Main.java
@@ -2,6 +2,8 @@ package exe_0;
 
 public class Main extends Thread{
     public static void main(String[] args){
-
+        Chef c1 = new Chef(500);
+        Chef c2 = new Chef(1000);
+        Chef c3 = new Chef(2000);
     }
 }
